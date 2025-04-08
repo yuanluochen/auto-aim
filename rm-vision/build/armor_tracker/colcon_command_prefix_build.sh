e42fc5e79be85e11aac29a8d6d83574170ab5842
yuanluochen <yuanluochen@foxmail.com>
2025-04-08 14:48:10 +0800

    update

---
+++ rm-vision/build/armor_tracker/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/yuanluochen/code/arming/rm-vision/install/auto_aim_interfaces/share/auto_aim_interfaces/package.sh"
---
+++ rm-vision/build/hik_camera/CMakeFiles/hik_camera_node.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hik_camera_node.
+# This may be replaced when dependencies are built.
