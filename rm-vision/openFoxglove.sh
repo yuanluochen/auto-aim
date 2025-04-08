10fede53333c7e106038575d92e98105be98b9be
yuanluochen <yuanluochen@foxmail.com>
2025-04-08 15:00:21 +0800

    update

--- rm-vision/openFoxglove.sh
+++ rm-vision/openFoxglove.sh
@@ -3,4 +3,4 @@ source /opt/ros/humble/setup.bash;\
 workdir=$(cd $(dirname $0); pwd)
 source $workdir/install/setup.bash;\
 
-ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765;
+ros2 launch foxglove_bridge foxglove_bridge_launch.xm-l port:=8765;
