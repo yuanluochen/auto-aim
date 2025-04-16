d294d4d035b7969e3a59e979a4107881531f53a0
yuanluochen <yuanluochen@foxmail.com>
2025-04-16 13:03:34 +0800

    更改文件结构

--- rebuild.sh
+++
@@ -1,6 +0,0 @@
-
-# delete
-workdir=$(cd $(dirname $0); pwd)
-rm -r $workdir/build/ $workdir/install/ $workdir/log/
-# build
-colcon build
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/ros/humble/setup.bash;\
-workdir=$(cd $(dirname $0); pwd)
-source $workdir/install/setup.bash;\
-
-ros2 launch rm_vision_bringup vision_bringup.launch.py;\
